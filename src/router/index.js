632f7ce7cc2d3b6b1e0dded354bd93a0ffc1c5db
lin971003 <313628497@qq.com>
2020-03-15 15:31:49 +0800

    项目结构搭建

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+//导入
+import Vue from 'vue'
+import vueRouter from 'vue-router'
+
+//使用  注册
+Vue.use(vueRouter);
+
+//实例化
+const router = new vueRouter({
+    routes:[
+
+    ]
+});
+
+//暴露路由
+export default router;
